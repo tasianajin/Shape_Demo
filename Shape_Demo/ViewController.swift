da62bb85a29c9e54ae96d0daf533125a9e4434d3
tasianajin <tasia.najin@gmail.com>
2021-09-24 07:23:07 +0600

    updates

--- Shape_Demo/ViewController.swift
+++ Shape_Demo/ViewController.swift
@@ -8,12 +8,96 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var startPoint: CGPoint?
+    var endPoint: CGPoint?
+    var startDot: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    var endDot: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+    let shapeLayer = CAShapeLayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            
+            if self.startPoint != nil {
+                let point = touch.location(in: view)
+                if shapeLayer.contains(point) {
+                    shapeLayer.strokeColor = UIColor.green.cgColor
+                }
+                return
+            }
+            let position = touch.location(in: view)
+            self.startPoint = position
+            startDot.center = position
+            startDot.backgroundColor = .red
+            view.addSubview(startDot)
+            let tap = UIPanGestureRecognizer(target: self, action: #selector(self.handleStartPointPan(gesture:)))
+            startDot.addGestureRecognizer(tap)
+            print(position)
+            
+        }
+    }
+    @objc func handleStartPointPan(gesture: UIPanGestureRecognizer) {
+        let newStart = gesture.location(in: view)
+        if let endPoint = self.endPoint {
+            startDot.center = newStart
+            self.drawLineFromPoint(start: gesture.location(in: view), toPoint: endPoint, ofColor: UIColor.blue, inView: self.view)
+            if gesture.state == UIGestureRecognizer.State.ended {
 
+                self.startPoint = gesture.location(in: view)
+             }
+        }
+        
 
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.endPoint != nil {
+            return
+        }
+        if let touch = touches.first, let startPoint = self.startPoint{
+            
+            let position = touch.location(in: view)
+            self.drawLineFromPoint(start: startPoint, toPoint: position, ofColor: UIColor.blue, inView: self.view)
+            self.endPoint = position
+            endDot.center = position
+            endDot.backgroundColor = .red
+            view.addSubview(endDot)
+            let tap = UIPanGestureRecognizer(target: self, action: #selector(self.handleEndPointPan(gesture:)))
+            endDot.addGestureRecognizer(tap)
+        }
+    }
+    @objc func handleEndPointPan(gesture: UIPanGestureRecognizer) {
+        let newEnd = gesture.location(in: view)
+        if let startPoint = self.startPoint {
+            endDot.center = newEnd
+            self.drawLineFromPoint(start: startPoint , toPoint: gesture.location(in: view), ofColor: UIColor.blue, inView: self.view)
+            if gesture.state == UIGestureRecognizer.State.ended {
+
+                self.endPoint = gesture.location(in: view)
+             }
+        }
+
+    }
+    
+    //Not sure how to do this part ???
+    func drawLineFromPoint(start : CGPoint, toPoint end:CGPoint, ofColor lineColor: UIColor, inView view:UIView) {
+        
+        shapeLayer.removeFromSuperlayer()
+
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addLine(to: end)
+        
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = 1.0
+        
+        view.layer.addSublayer(shapeLayer)
+        
+    }
+    
 }
 
